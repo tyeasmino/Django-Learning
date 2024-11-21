fdd6540d90181d06a443a84aea8c9161fa90feb5
Tamima Yeasmin <tyeasmino@gmail.com>
2024-11-21 09:32:47 +0600

    FH: Module 22.5 Done

--- Week 06 - Bank Management Project/Module 22 - Bank Management Project Part-II/mamar_bank/transactions/views.py
+++ Week 06 - Bank Management Project/Module 22 - Bank Management Project Part-II/mamar_bank/transactions/views.py
@@ -37,3 +37,32 @@ class TransactionCreateMixin(LoginRequiredMixin, CreateView):
 
         return context
 
+
+
+class DepositMoneyView(TransactionCreateMixin):
+    form_class = DepositForm
+    title = 'Deposit'
+
+    def get_initial(self):
+        initial = {'transaction_type': DEPOSIT}
+        return initial
+
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        # if not account.initial_deposit_date:
+        #     now = timezone.now()
+        #     account.initial_deposit_date = now
+        account.balance += amount # amount = 200, tar ager balance = 0 taka new balance = 0+200 = 200
+        account.save(
+            update_fields=[
+                'balance'
+            ]
+        )
+
+        messages.success(
+            self.request,
+            f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully'
+        )
+
+        return super().form_valid(form)
